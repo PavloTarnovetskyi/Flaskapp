b8abac1b6814f21bd26d978ed5a2e337b63ad497
Pavlo Tarnovetsky <pavlotarnovetskyi@gmail.com>
2022-09-30 10:09:44 +0300

    playbook - added 2 tasks (pull image & create container with node-exporter)

--- app&dockerfile/app.py
+++ app&dockerfile/app.py
@@ -5,23 +5,12 @@ from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 
-
-metrics = PrometheusMetrics(app)
-metrics.start_http_server(9100, host="0.0.0.0")
-
-
 ssm = boto3.client('ssm', region_name='eu-north-1')
-  
 APP_ID = ssm.get_parameter(Name='APP_ID')['Parameter']['Value']
-
-
-
 ENDPOINT = "https://openexchangerates.org/api/latest.json"
 CURRENCY_DEF_ENDPOINT = "https://openexchangerates.org/api/currencies.json"
-
 exchange_rates = requests.get(f"{ENDPOINT}?app_id={APP_ID}").json()["rates"]
 currency_definition = requests.get(f"{CURRENCY_DEF_ENDPOINT}").json()
-
 
 # define the  URL on application
 @app.route('/', methods=['GET', 'POST'])
@@ -40,8 +29,6 @@ def home():
 @app.route('/diagram/', methods=['GET', 'POST'])
 def diagram():
     return render_template('diagram.jinja2')
-    
-
 
 # To start the server, and allow the flask receive connection.
 if __name__ == '__main__':
