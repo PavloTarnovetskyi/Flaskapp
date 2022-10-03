ae5e5ace41496715f8b3d3dfe066f327ae9e630b
PavloTarnovetskyi <98552183+PavloTarnovetskyi@users.noreply.github.com>
2022-10-03 18:31:22 +0300

    deleted one backslash

--- terraform/script.sh
+++ terraform/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 Public_IP=`terraform output public_ip | sed 's/.\\(.*\\)/\\1/' | sed 's/\\(.*\\)./\\1/'`
 echo $Public_IP
-sed -i -e "s/'\\(.*\\):9100'/'$Public_IP:9100'/g" /usr/local/etc/prometheus.yml
+sed -i -e "s/'\(.*\\):9100'/'$Public_IP:9100'/g" /usr/local/etc/prometheus.yml
 brew services restart prometheus
