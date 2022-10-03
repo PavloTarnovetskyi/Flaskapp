9cda6cc9c77e31fd419ee767dae95ce8db4b437c
PavloTarnovetskyi <98552183+PavloTarnovetskyi@users.noreply.github.com>
2022-10-03 18:21:13 +0300

    Update terraform/script.sh
    Co-authored-by: nos373737 <nos373737@gmail.com>

--- terraform/script.sh
+++ terraform/script.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 Public_IP=`terraform output public_ip | sed 's/.\\(.*\\)/\\1/' | sed 's/\\(.*\\)./\\1/'`
 echo $Public_IP
 sed -i -e "s/'\\(.*\\):9100'/'$Public_IP:9100'/g" /usr/local/etc/prometheus.yml
